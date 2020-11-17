830e4a6fc0d3287a3069a6b0e7d5fa43f7e7f0f3
7Nic <gabriel.nicolau97@hotmail.com>
2020-11-17 15:09:44 -0200

    Added function to build distance matrix

--- convert.py
+++ convert.py
@@ -1,16 +1,26 @@
 import numpy as np
+import math
 
-f = open("./data/qatar.tsp", "r")
+def distance(x1,y1,x2,y2):
+  return math.sqrt((x1-x2)**2 + (y1-y2)**2)
+
+# f = open("./data/teste.tsp", "r")
 qttLines = int(f.readline())
 distances = np.zeros([qttLines,qttLines], dtype=float)
 points = []
-print(distances)
 for i in range(qttLines):
   line = f.readline()
   (x,y) = line.split()
   x = float(x)
   y = float(y)
   points.append((x,y))
-  # print(x, y)
 
-print(points)
+for i in range(qttLines):
+  for j in range(qttLines):
+    if (i == j): 
+      distances[i][j] = 100000000000
+      continue
+
+    distances[i][j] = distance(points[i][0],points[i][1],points[j][0],points[j][1])
+
+print(distances)
