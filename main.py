6d601064fd18c9fa8cc9857484af58dd03b1d78d
7Nic <gabriel.nicolau97@hotmail.com>
2020-12-04 12:09:34 -0200

    Fixing last edge when plotting

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import math
 import utils
 import dlModel as dl
 
-FILE = "qatar"
+FILE = "djibouti"
 start_time = time.time()*1000
 
 def main():
@@ -52,7 +52,7 @@ def SolveModel(solver, num_nodes, modelVars, costs, points):
     print("Starting...")
     minutes = 10*60*1000 #10 minutes
     seconds = 20*1000 # 20 seconds
-    solver.set_time_limit(minutes)  # Time in ms
+    solver.set_time_limit(seconds)  # Time in ms
     status = solver.Solve()
     print('Finished')
 
@@ -62,17 +62,21 @@ def SolveModel(solver, num_nodes, modelVars, costs, points):
     if status == pywraplp.Solver.OPTIMAL or status == pywraplp.Solver.FEASIBLE:
 
         i = 0
-        # Iterate num_galaxies times
         for k in range(num_nodes):
             for j in range(num_nodes):
                 # Test if x[i,j] is 1 (with tolerance for floating point arithmetic).
                 if modelVars[i, j].solution_value() > 0.5:
                     pathX.append(points[i][0])
                     pathY.append(points[i][1])
-                    print('Galaxy %d to galaxy %d.  Cost = %d' %
+                    print('Node %d to node %d.  Cost = %d' %
                           (i, j, costs[i][j]))
                     i = j
                     break
+
+        # Last edge to plot
+        pathX.append(points[0][0])
+        pathY.append(points[0][1])
+
 
         print('Total cost = ', solver.Objective().Value(), '\n')
         plt.plot(pathX, pathY, 'bo-', zorder=2)
@@ -82,21 +86,7 @@ def SolveModel(solver, num_nodes, modelVars, costs, points):
         print("It's not feasible")
 
     milliseconds = time.time()*1000 - start_time
-    print("Execution time:", milliseconds/1000, "s")
+    print("Total execution time:", milliseconds/1000, "s")
     
-
-
-
-
 if __name__ == "__main__":
     main()
-
-# https://developers.google.com/optimization
-# https://google.github.io/or-tools/python/ortools/linear_solver/pywraplp.html
-# https://developers.google.com/optimization/mip/integer_opt
-# https://www.scipopt.org/doc/html/
-
-# Podemos fazer o modelo aqui em python e exportar em formato .lp para rodar no programa do SCIP: ExportModelAsLpFormat - https://developers.google.com/optimization/reference/python/linear_solver/pywraplp
-
-# Para poder usar o SCIP pelo shell
-# https://www.scipopt.org/doc/html/SHELL.php
